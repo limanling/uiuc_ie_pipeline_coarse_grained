4e820ce69a8ab7438027e76bf6bc559cf5ad5a40
Qingyun Wang <dalewanghz@gmail.com>
2019-01-27 21:46:46 -0500

    Update gail_nominal_test.py

--- aida_nominal_coreference_en/gail_nominal_test.py
+++ aida_nominal_coreference_en/gail_nominal_test.py
@@ -17,10 +17,22 @@ parser.add_argument(
     "--out_e", default="en.linking.tab",
     help="Output edl location"
 )
-args = vars(parser.parse_args())
-json_string = json.dumps(args)
+args = parser.parse_args()
+print('Loading tagged docs from %s...' % args.dev)
+dev = open(args.dev, 'r').read().split("\n")
+while len(dev[-1]) == 0:
+    dev = dev[:-1]
+print('Loading edl mentions from %s...' % args.dev_e)
+dev_e = open(args.dev_e, 'r').read().split("\n")
+while len(dev_e[-1]) == 0:
+    dev_e = dev_e[:-1]
+result = {'dev':dev, 'dev_e':dev_e}
+json_string = json.dumps(result)
 r = requests.post('http://127.0.0.1:2468/aida_nominal_coreference_en', json=json_string)
 if r.status_code == 200:
     print("Succeed coreference")
+    f = open(args.out_e, 'w')
+    f.write(r.text)
+    f.close()
 else:
     print(r.status_code)
